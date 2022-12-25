82f450d6cdd8e80c2396de1d87208adcc368c063
ritesh rijal <92074675+RiteshRijal@users.noreply.github.com>
2022-12-25 06:57:24 +0545

    Add files via upload

---
+++ ObjectTest.java
@@ -0,0 +1,10 @@
+public class ObjectTest
+{
+    private int a=10;
+    private int b=20;
+
+    public static void main(String[] args) {
+        ObjectTest ot=new ObjectTest();//object & constructor
+        System.out.println(ot.a);
+    }
+}
